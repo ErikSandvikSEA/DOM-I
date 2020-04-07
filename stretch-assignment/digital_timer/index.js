8ca4cef4732efb0cc2d77cd426798008c3883376
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-06 17:51:39 -0700

    Got timer to go up, now just need it to stop and turn red

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -4,13 +4,19 @@ let secondTens = document.querySelector('#secondTens')
 let secondOnes = document.querySelector('#secondOnes')
 let msHundreds = document.querySelector('#msHundreds')
 let msTens = document.querySelector('#msTens')
+let digits = document.querySelectorAll('.digits')
+
 console.log(secondTens)
 
 function increaseTime(id, interval) {
      id.textContent = 0;
-     setInterval(function (){
-       id.textContent++
-     }, interval     
+     setInterval(function () {
+          if(id.textContent < 9){
+          id.textContent++
+          } else {
+               id.textContent = 0
+          }
+     }, interval
      )
 }
 
@@ -18,3 +24,10 @@ increaseTime(secondTens, 10000)
 increaseTime(secondOnes, 1000)
 increaseTime(msHundreds, 100)
 increaseTime(msTens, 10)
+
+let addRed = function(){
+     if(secondTens.textContent >= 1){
+          digits.classList.add('.redDigit')
+     }
+}
+addRed()
